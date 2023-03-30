75d058803398d7e153b09b60198410eb87357faa
tuna0426 <my910426@gmail.com>
2023-03-31 01:52:55 +0800

    0330 all test vertion

--- test.py
+++ test.py
@@ -1,86 +1,22 @@
 import cv2
 
-# 導入鋼琴樂譜圖片
-img = cv2.imread("sheet_music.png")
-
-# 將圖像轉換為灰度圖像
+# 讀取圖片
+img = cv2.imread("piano.png")
+img = cv2.resize(img, (1200, 600))
+# 將圖片轉為灰階
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 二值化處理
-_, thresh = cv2.threshold(
-    gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-# 找出輪廓
-contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# 將輪廓存儲為列表
-key_contours = []
-for contour in contours:
-    area = cv2.contourArea(contour)
-    if area > 50:
-        key_contours.append(contour)
-# 定義函數來識別黑鍵和白鍵
+# 二值化圖片
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
+# 尋找輪廓
+# contours, hierarchy = cv2.findContours(
+#    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-def is_black_key(contour):
-    x, y, w, h = cv2.boundingRect(contour)
-    aspect_ratio = float(w) / h
-    if aspect_ratio < 0.5:
-        return True
-    else:
-        return False
+# 繪製輪廓
+#cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
 
-
-# 分類黑鍵和白鍵
-black_keys = []
-white_keys = []
-for contour in key_contours:
-    if is_black_key(contour):
-        black_keys.append(contour)
-    else:
-        white_keys.append(contour)
-
-# 存儲鍵的位置信息
-key_positions = {}
-for key in black_keys:
-    x, y, w, h = cv2.boundingRect(key)
-    key_positions[x] = "black"
-for key in white_keys:
-    x, y, w, h = cv2.boundingRect(key)
-    key_positions[x] = "white"
-# 定義函數來識別黑鍵和白鍵
-
-
-def is_black_key(contour):
-    x, y, w, h = cv2.boundingRect(contour)
-    aspect_ratio = float(w) / h
-    if aspect_ratio < 0.5:
-        return True
-    else:
-        return False
-
-
-# 分類黑鍵和白鍵
-black_keys = []
-white_keys = []
-for contour in key_contours:
-    if is_black_key(contour):
-        black_keys.append(contour)
-    else:
-        white_keys.append(contour)
-
-# 存儲鍵的位置信息
-key_positions = {}
-for key in black_keys:
-    x, y, w, h = cv2.boundingRect(key)
-    key_positions[x] = "black"
-for key in white_keys:
-    x, y, w, h = cv2.boundingRect(key)
-    key_positions[x] = "white"
-# 讀取鏡頭
-cap = cv2.VideoCapture(0)
-
-while True:
-    # 抓取鏡頭圖像
-    ret, frame = cap.read()
-
-    # 將圖像轉換為灰度圖像
-    gray = cv2
+# 顯示圖片
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- test2.py
+++ test2.py
@@ -1,37 +1,60 @@
 import cv2
-import numpy as np
 
-# 定義初始位置和大小
-x, y = 100, 100
-w, h = 50, 50
+# 鋼琴黑鍵和白鍵的寬度比例
+BLACK_WHITE_RATIO = 0.6
 
-# 創建一個黑色畫布
-canvas = 255 * np.ones((480, 640, 3), dtype=np.uint8)
+# 音符名稱列表
+NOTE_NAMES = ["C", "D", "E", "F", "G", "A", "B"]
 
-# 創建一個窗口
-cv2.namedWindow('Square')
+# 讀取圖片
+img = cv2.imread("piano.png")
 
-# 移動方形
-while True:
-    # 繪製方形
-    cv2.rectangle(canvas, (x, y), (x + w, y + h), (0, 0, 255), -1)
+# 將圖片轉為灰階
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 在窗口中顯示方形
-    cv2.imshow('Square', canvas)
+# 二值化圖片
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
-    # 等待一段時間
-    cv2.waitKey(50)
+# 尋找輪廓
+contours, hierarchy = cv2.findContours(
+    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 將方形向右移動
-    x += 5
+# 計算黑白鍵的寬度
+keys = []
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    if w < 10 or h < 10:
+        continue
+    if float(w) / h < BLACK_WHITE_RATIO:
+        # 黑鍵
+        keys.append((x, y, w, h, "black"))
+    else:
+        # 白鍵
+        keys.append((x, y, w, h, "white"))
 
-    # 如果方形移出窗口，重新從左側開始移動
-    if x >= canvas.shape[1]:
-        x = 0 - w
+# 以鍵位的x座標為基礎，排序所有鍵位
+keys = sorted(keys, key=lambda k: k[0])
 
-    # 如果按下 ESC 鍵，退出程式
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+# 計算每個鍵位對應的音符名稱
+notes = []
+for i, key in enumerate(keys):
+    if key[4] == "white":
+        note_idx = i // 7
+        note_name = NOTE_NAMES[note_idx]
+        octave = (i // 7) + 1
+        notes.append((key[0], key[1], key[2], key[3], note_name + str(octave)))
+    else:
+        notes.append((key[0], key[1], key[2], key[3], ""))
 
-# 釋放資源
+# 繪製鍵位和音符名稱
+for note in notes:
+    x, y, w, h, note_name = note
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    if note_name:
+        cv2.putText(img, note_name, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+# 顯示圖片
+cv2.imshow("Image", img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
