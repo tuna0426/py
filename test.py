12910fd4a090a4375d29d8042d7c8b1c6fe56ccf
tuna0426 <my910426@gmail.com>
2023-03-31 02:08:59 +0800

    push test 2
    Signed-off-by: tuna0426 <my910426@gmail.com>

--- test.py
+++ test.py
@@ -10,11 +10,11 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
 # 尋找輪廓
-# contours, hierarchy = cv2.findContours(
-#    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(
+    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # 繪製輪廓
-#cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
 
 # 顯示圖片
 cv2.imshow("Image", img)
